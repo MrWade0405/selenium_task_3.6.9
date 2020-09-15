5e6f8bf19aadd94be37bfc981ab08d370bf7c784
MahlonaVitalii <maglona2013@gmail.com>
2020-09-15 14:28:21 +0300

    completed task

---
+++ test_items.py
@@ -0,0 +1,16 @@
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+# import time
+
+def test_language(browser):
+    browser.get(link)
+	# to check the correctness of work
+    # time.sleep(30)
+    button = None
+	
+    try:
+        button = browser.find_element_by_css_selector(".btn-add-to-basket")
+    except ZeroDivisionError:
+        # don't need to be processed here, 'assert' is called below
+        pass
+
+    assert button != None, "The button for adding an item to the shopping cart doesn't exist"
